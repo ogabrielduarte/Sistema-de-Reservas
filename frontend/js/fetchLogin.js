0d2bd94d3e34e1a7e58668440e1ada94342c2853
ogabrielduarte <gabriel.sduarte.oli@gmail.com>
2026-06-06 20:15:38 -0300

    update log-in fetch

---
+++ frontend/js/fetchLogin.js
@@ -0,0 +1,19 @@
+export async function login(email, senha) {
+    const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, senha })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.erro);
+    }
+
+    localStorage.setItem("token", data.token);
+
+    return data;
+}
